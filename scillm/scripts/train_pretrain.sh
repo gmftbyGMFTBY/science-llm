fe159e87e4d405bface3ce54925b616585cf58fd
GMFTBY <18811371908@163.com>
2023-05-29 20:28:41 +0800

    Update train_pretrain.sh

--- scillm/scripts/train_pretrain.sh
+++ scillm/scripts/train_pretrain.sh
@@ -3,7 +3,7 @@
 deepspeed --include localhost:0 --master_addr 127.0.0.1 --master_port 28457 train_pretrain.py \
     --model scillm\
     --model_path /home/johnlan/pretrained_models/LLaMA-7B-HF/ \
-    --train_data_path ../data/redpajama_tokens.json \
+    --train_data_path ../data/redpajama_test_tokens.json \
     --test_data_path ../data/redpajama_test_tokens.json \
     --save_path ./ckpt/scillm/ \
     --log_path ./rest/
